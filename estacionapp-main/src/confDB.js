63fa0144ce53b0263a65a467987c5bf7bb981b51
AngelMelendres <fabian_melendres@hotmail.com>
2024-03-01 11:27:28 -0500

    app crate

---
+++ estacionapp-main/src/confDB.js
@@ -0,0 +1,9 @@
+module.exports = {
+  database: {
+    user: "postgres",
+    host: "roundhouse.proxy.rlwy.net",
+    password: "F1C*aEeFC6B226a1g51B-a**AFBBGBA5",
+    database: "railway",
+    port: 30411,
+  },
+};
